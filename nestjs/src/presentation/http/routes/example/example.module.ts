6fe62e002d0fa68c294285c96ab48a1f2af423c1
Wendell Bitencourt <bat.delgeorge@hotmail.com>
2026-02-12 22:23:54 -0300

    update

--- nestjs/src/presentation/http/routes/example/example.module.ts
+++ nestjs/src/presentation/http/routes/example/example.module.ts
@@ -2,7 +2,8 @@ import { Module } from '@nestjs/common';
 import { ExampleController } from '@/presentation/http/routes/example/example.controller';
 import { ExampleService } from '@/presentation/http/routes/example/example.service';
 import { DatabaseModule } from '@/database/typeorm-database.module';
-import { TypeOrmExampleRepository } from '@/database/repositories/typeorm-example.repository';
+import { PrismaExampleRepository } from '@/database/repositories/prisma-example.repository';
+//import { TypeOrmExampleRepository } from '@/database/repositories/typeorm-example.repository';
 import { ExampleRepository } from '@/database/repositories/example.repository';
 
 @Module({
@@ -12,7 +13,7 @@ import { ExampleRepository } from '@/database/repositories/example.repository';
     ExampleService,
     {
       provide: ExampleRepository,
-      useClass: TypeOrmExampleRepository,
+      useClass: PrismaExampleRepository,
     },
   ],
 })
